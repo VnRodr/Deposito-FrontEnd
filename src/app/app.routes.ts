5f7e3d3102a1ebbf9d29866bf7a4fbccd7f940bc
VnRodr <viniciur@protonmail.com>
2025-10-19 15:18:02 -0300

    Adição de novas rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,11 @@
 import { Routes } from '@angular/router';
 import { PaginaInicial } from './components/initial_layout/pagina-inicial/pagina-inicial';
 import { CreateUser } from './components/pages/user-page/create-user/create-user';
+import { CreateItem } from './components/pages/create-item/create-item';
 
 export const routes: Routes = [
-  { 
-    path: '', 
-    component:  PaginaInicial,
-    children:[
-    {path: 'user/create-user', component: CreateUser}
-    ]
-  }
-
+  {path: '', component:  PaginaInicial},
+  {path: 'user/create-user', component: CreateUser},
+  {path: 'item/create-item', component: CreateItem}
 ];
 
