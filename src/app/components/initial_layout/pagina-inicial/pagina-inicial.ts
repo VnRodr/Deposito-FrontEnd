e8d4001b6a67ba8299e4801464c9d5014457b9e7
VnRodr <viniciur@protonmail.com>
2025-10-19 15:22:05 -0300

    Criando um botão para navegar entre a criação de usuários e itens

--- src/app/components/initial_layout/pagina-inicial/pagina-inicial.ts
+++ src/app/components/initial_layout/pagina-inicial/pagina-inicial.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { PaginaInicialHeader } from './pagina-inicial-header/pagina-inicial-header';
+import { FormList } from "../../pages/form-list/form-list";
+import { SelectButton } from 'primeng/selectbutton';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-pagina-inicial',
-  imports: [ButtonModule],
+  imports: [ButtonModule, SelectButton, FormsModule, RouterOutlet],
   standalone:true,
   templateUrl: './pagina-inicial.html',
   styleUrl: './pagina-inicial.scss'
 })
 export class PaginaInicial {
+  pageSelection: any[] = [{ label: 'Criar usuário', value: 'criarUsuario' },{ label: 'Criar item', value: 'criarItem' }];
 
+  value: string = 'off';
+
+  constructor(private router: Router) {}
+
+  onClick() {
+    if (this.value === 'criarUsuario') {
+      this.router.navigate(['user/create-user']);
+    } else if (this.value === 'criarItem') {
+      this.router.navigate(['item/create-item']);
+    }
+  }
 }
