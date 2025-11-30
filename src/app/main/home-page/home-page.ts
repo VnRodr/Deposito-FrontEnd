09a6d268e62d933c207a890a7618d3413b059a6e
VnRodr <viniciur@protonmail.com>
2025-11-30 15:55:53 -0300

    Criação dashboard

--- src/app/main/home-page/home-page.ts
+++ src/app/main/home-page/home-page.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from "@angular/router";
 import { DashboardComponent } from "../../components/dashboard/dashboard";
+import { CardShelve } from "../../components/card-shelve/card-shelve";
 
 @Component({
   selector: 'app-home-page',
-  imports: [DashboardComponent],
+  imports: [DashboardComponent, CardShelve],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss'
 })
