1b964c961ba8b67d7a66e10adac9e45bb2a91981
VnRodr <viniciur@protonmail.com>
2025-12-14 14:56:40 -0300

    Atualização do dashboard: 1. Término do estilo 2. Adição de bindinds os campos de input

--- src/app/components/dashboard/dashboard.ts
+++ src/app/components/dashboard/dashboard.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { LucideAngularModule, Package } from 'lucide-angular';
+import { Dialog } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [LucideAngularModule],
+  imports: [LucideAngularModule, Dialog, ButtonModule, InputTextModule, FormsModule],
   standalone: true,
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss'
 })
 export class DashboardComponent {
   readonly Package = Package;
+
+  stockGroup!: StockGroup;
+  employee!: Employee;
+
+  visible: boolean = false;
+
+  showCreateStockGroupForm() {
+    this.visible = true;
+  }
+
+
 }
