8e43d11167c3a4064f82c2956fbb86b0f841088d
VnRodr <viniciur@protonmail.com>
2026-01-08 21:18:23 -0300

    Stilying stock itens table -> The table will keep a specific space in the screen

--- src/app/components/stock-groups/stock-itens/stock-itens.ts
+++ src/app/components/stock-groups/stock-itens/stock-itens.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { LucideAngularModule, Package } from 'lucide-angular';
+import { LucideAngularModule, LucideIconData, Package, SquarePen, Trash } from 'lucide-angular';
 import { StockAndStatus } from '../stock-and-status/stock-and-status';
 import { Item } from '../../../data/item';
 import { Status } from '../../../enums/status';
@@ -13,6 +13,8 @@ import { Status } from '../../../enums/status';
 })
 export class StockItens {
   readonly Package = Package;
+  readonly SquarePen: LucideIconData = SquarePen;
+  readonly Trash: LucideIconData = Trash;
 
   stockData: StockAndStatus = new StockAndStatus();
   item!: Item; status!: Status;
